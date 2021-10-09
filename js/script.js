84448052b88b30cab64340298c80debf1078ce78
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2021-10-09 03:19:35 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let noInfo = 'Жаль, что Вы не захотели ввести свой(ю)';
+let year = prompt('Введите свой год рождения:');
+switch (year) {
+    case null:
+        year = noInfo + ' ' + 'год рождения';
+        alert(year);
+        break;
+    default: 
+        if (year) {
+            age = 2021 - year;
+            let town = prompt('Введите свой город:');
+            switch (town) {
+                case null:
+                    town = noInfo + ' ' + 'город';
+                    alert(town);
+                    break;
+                case 'Киев':
+                    town = 'Ты живешь в столице Украины';
+                    break;
+                case 'Москва':
+                    town = 'Ты живешь в столице России';
+                    break;
+                case 'Минск':
+                    town = 'Ты живешь в столице Беларуссии';
+                    break;
+                default:
+                    town = 'Ты живешь в городе'+' '+ town;
+            }
+                let hobby = prompt('Какой ваш любимый вид спорта?');
+                switch (hobby) {
+                    case null:
+                        hobby = noInfo + 'любимый вид спорта';
+                        alert(hobby);
+                        break;
+                    case 'плавание':
+                        hobby = 'Круто! Хочешь стать Майклом?';
+                        break;
+                    case 'гребля':
+                        hobby = 'Круто! Хочешь стать Сергеем?';
+                        break;
+                    case 'бокс':
+                        hobby = 'Круто! Хочешь стать Александром?';
+                        break;
+                    default:
+                        hobby = 'Любимый вид спорта' + ' ' + hobby;
+                }
+            alert('Тебе '+ age + ' лет' + '\n'+ town + '\n'+ hobby);
+            
+        }
+}
